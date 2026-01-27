896fbddbf83c506d485d6c9f0704120ec9ce6f8a
breverdbidder <brevardbidderai@gmail.com>
2026-01-27 18:23:02 -0500

    Add components index

---
+++ components/index.ts
@@ -0,0 +1,4 @@
+export { default as EnterpriseLayout } from './enterprise/EnterpriseLayout'
+export { default as SessionSidebar } from './enterprise/SessionSidebar'
+export { default as ChatPanel } from './enterprise/ChatPanel'
+export { default as ArtifactPanel } from './enterprise/ArtifactPanel'
