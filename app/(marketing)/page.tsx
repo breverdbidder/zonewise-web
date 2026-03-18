7de07d8ecc00153e35d8a0e445e19afefd870af5
breverdbidder <brevardbidderai@gmail.com>
2026-03-18 06:20:47 -0400

    revert: remove force-dynamic from client page (wrong approach)

--- app/(marketing)/page.tsx
+++ app/(marketing)/page.tsx
@@ -1,7 +1,5 @@
 'use client'
 
-export const dynamic = 'force-dynamic'
-
 import Link from 'next/link'
 import SplitScreenPreview from '@/components/SplitScreenPreview'
 import BetaSignupForm from '@/components/BetaSignupForm'
