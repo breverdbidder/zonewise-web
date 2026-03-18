f58e6758fd582314e6416375e21a6699af16db65
breverdbidder <brevardbidderai@gmail.com>
2026-03-18 06:20:19 -0400

    fix: add force-dynamic to marketing page to fix Clerk SSR prerender error

--- app/(marketing)/page.tsx
+++ app/(marketing)/page.tsx
@@ -1,5 +1,7 @@
 'use client'
 
+export const dynamic = 'force-dynamic'
+
 import Link from 'next/link'
 import SplitScreenPreview from '@/components/SplitScreenPreview'
 import BetaSignupForm from '@/components/BetaSignupForm'
